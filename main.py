0592fdad2e5b611b6756ed1f2a92f4d9e5e62013
Matt <109485946+mattlgr@users.noreply.github.com>
2022-07-31 14:32:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from geoip import *
+import sys
 
 ip = input("Desired IP> ")
 
 api = GeoIP(ip)
+
+if api.validate_ipv4() == None:
+    print("Invalid IP given!")
+    sys.exit(0)
+
 api.requestApi()
 api.parseResponse()
 api.buildResponse()
